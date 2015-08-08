a77142317643752905ea0fec893bda321fb0afce
Martin <martin.l.bayer@gmail.com>
2015-08-08 05:30:45 +0200

    Task 2 - added mobile requirements

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,4 +4,29 @@
  * @author Martin Bayer
  * @date 07 August 2015
  */
+(function(){
+    var nav = $('nav'),
+        content = $('.content'),
+        sub = $('.js-sub');
 
+    // mobile navigation
+    $('.mobile-nav').on('click', function() {
+
+        if(!nav.hasClass('js-drawer-open')){
+            nav.addClass('js-drawer-open');
+            content.addClass('js-moved');
+
+        } else {
+            nav.removeClass('js-drawer-open');
+            content.removeClass('js-moved');
+        }
+    });
+
+    // sub navigation
+    if($('.mobile-nav:visible')){
+        sub.on('click', function(){
+            sub.find('ul:visible').slideUp('fast');
+            $(this).find('ul').slideDown('fast');
+        });
+    }
+})();
