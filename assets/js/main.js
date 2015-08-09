883759f2b34c578d3d5ccfc157eca5bec0541b28
Martin <martin.l.bayer@gmail.com>
2015-08-10 00:26:58 +0200

    Task 2 - started with pin app

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,15 +4,39 @@
  * @author Martin Bayer
  * @date 07 August 2015
  */
-(function(){
+(function () {
+    'use strict';
+
+    // pin stuff
+    var items = $('.pin-item'),
+        contentWidth = $('.content').innerWidth(),
+        chunks = [],
+        gap = 20;
+
+    // calc how many items fit in content width
+    function getGridSize() {
+        var columns = Math.floor(contentWidth / (items.outerWidth() + gap));
+
+        
+
+        console.log(columns);
+    }
+
+
+    getGridSize();
+
+
+    /**
+     * Small stuff
+     */
     var nav = $('nav'),
         content = $('.content'),
         sub = $('.js-sub');
 
     // mobile navigation
-    $('.mobile-nav').on('click', function() {
+    $('.mobile-nav').on('click', function () {
 
-        if(!nav.hasClass('js-drawer-open')){
+        if (!nav.hasClass('js-drawer-open')) {
             nav.addClass('js-drawer-open');
             content.addClass('js-moved');
 
@@ -23,8 +47,8 @@
     });
 
     // sub navigation
-    if($('.mobile-nav:visible')){
-        sub.on('click', function(){
+    if ($('.mobile-nav:visible')) {
+        sub.on('click', function () {
             sub.find('ul:visible').slideUp('fast');
             $(this).find('ul').slideDown('fast');
         });
